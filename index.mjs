0d00ad5db7a4537ed01b2b5ebd08df61b88470f3
Niverton Felipe Pereira Sousa, Minsait Brasil LTDA <nfelipepereirasousa@minsait.com>
2024-10-03 16:30:49 -0300

    Adicionando variável de ambiente no LOG

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ import { log } from './log.mjs';
 
 export const handler = async (event) => {
 
-  log('Log de execução após GitHub Action. Event: ' + JSON.stringify(event))
+  log('Event: ' + JSON.stringify(event))
   
   return {
     statusCode: 200,
